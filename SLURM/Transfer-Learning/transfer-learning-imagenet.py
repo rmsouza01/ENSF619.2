6d9686d58e068cc020d9ce4eef5c680efc09f9d7
rmsouza01 <roberto.medeiros.souza@gmail.com>
2021-10-10 00:30:37 -0600

    TL cluster example

---
+++ SLURM/Transfer-Learning/transfer-learning-imagenet.py
@@ -0,0 +1,133 @@
+import tensorflow as tf
+print(tf.__version__)
+import numpy as np
+import glob
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import EfficientNetB0
+import pickle
+
+
+# Data generator parameters
+gen_params = {"featurewise_center":False,\
+              "samplewise_center":False,\
+              "featurewise_std_normalization":False,\
+              "samplewise_std_normalization":False,\
+              "zca_whitening":False,\
+              "rotation_range":20,\
+              "width_shift_range":0.1,\
+              "height_shift_range":0.1, \
+              "shear_range":0.2, \
+              "zoom_range":0.1,\
+              "horizontal_flip":True,\
+              "vertical_flip":True}
+
+# Train and validation generators
+train_gen = ImageDataGenerator(**gen_params, preprocessing_function = tf.keras.applications.efficientnet.preprocess_input)
+val_gen = ImageDataGenerator(**gen_params, preprocessing_function = tf.keras.applications.efficientnet.preprocess_input)
+
+class_names = ["Black", "Blue",  "Green", "Take to recycle"]
+
+
+
+train_generator = train_gen.flow_from_directory(
+    directory = "/home/roberto.medeirosdeso/Transfer-Learning/Garbage-dataset/Train",
+    target_size=(256, 256),
+    color_mode="rgb",
+    classes= class_names,
+    class_mode="categorical",
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    interpolation="nearest",
+)
+
+validation_generator = val_gen.flow_from_directory(
+    directory = "/home/roberto.medeirosdeso/Transfer-Learning/Garbage-dataset/Validation ",
+    target_size=(256, 256),
+    color_mode="rgb",
+    classes= class_names,
+    class_mode="categorical",
+    batch_size=32,
+    shuffle=True,
+    seed=42,
+    interpolation="nearest",
+)
+
+model_name_it = "/home/roberto.medeirosdeso/Transfer-Learning/Output/garbage_classifier_en_b0_it.h5"
+model_name_ft = "/home/roberto.medeirosdeso/Transfer-Learning/Output/garbage_classifier_en_b0_ft.h5"
+
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience = 20)
+
+
+monitor_it = tf.keras.callbacks.ModelCheckpoint(model_name_it, monitor='val_loss',\
+                                             verbose=0,save_best_only=True,\
+                                             save_weights_only=False,\
+                                             mode='min')
+
+monitor_ft = tf.keras.callbacks.ModelCheckpoint(model_name_ft, monitor='val_loss',\
+                                             verbose=0,save_best_only=True,\
+                                             save_weights_only=False,\
+                                             mode='min')
+
+def scheduler(epoch, lr):
+    if epoch%10 == 0 and epoch!= 0:
+        lr = lr/2
+    return lr
+
+lr_schedule = tf.keras.callbacks.LearningRateScheduler(scheduler,verbose = 0)
+
+
+img_height = 256
+img_width = 256
+
+
+# Defining the model
+base_model = tf.keras.applications.EfficientNetB0(
+    weights='imagenet',  # Load weights pre-trained on ImageNet.
+    input_shape=(img_height, img_width, 3),
+    include_top=False) 
+base_model.trainable = False
+
+x1 = base_model(base_model.input, training = False)
+x2 = tf.keras.layers.Flatten()(x1)
+out = tf.keras.layers.Dense(len(class_names),activation = 'softmax')(x2)
+model = tf.keras.Model(inputs = base_model.input, outputs =out)
+
+print("Initial Training Model")
+print(model.summary())
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate = 1e-4),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+history_it = model.fit(train_generator, epochs=100, verbose = 1, \
+                       callbacks= [early_stop, monitor_it, lr_schedule], \
+                       validation_data = (validation_generator))
+
+it_file = open("/home/roberto.medeirosdeso/Transfer-Learning/Output/it_history.pkl", "wb")
+pickle.dump(history_it, it_file)
+it_file.close()
+
+# Fine-tuning the model
+model = tf.keras.models.load_model(model_name_it)
+model.trainable = True
+
+print("Fine-tuning model")
+print(model.summary())
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate = 1e-8),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+history_ft = model.fit(train_generator, epochs=50, verbose = 1, \
+                       callbacks= [early_stop, monitor_ft, lr_schedule], \
+                       validation_data = (validation_generator))
+
+ft_file = open("/home/roberto.medeirosdeso/Transfer-Learning/Output/ft_history.pkl", "wb")
+pickle.dump(history_it, it_file)
+ft_file.close()
+
+
+
